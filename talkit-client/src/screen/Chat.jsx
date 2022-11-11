84f73e172df410ec594e56f94febfd7a97d40222
Doddy MATABARO <matabarododdy@gmail.com>
2022-11-11 11:33:02 +0200

    chat styled : seach box

--- talkit-client/src/screen/Chat.jsx
+++ talkit-client/src/screen/Chat.jsx
@@ -59,6 +59,27 @@ function Chat() {
                                             </a>
                                         </div>
                                     </div>
+
+                                    <div className='mb-4'>
+                                        <div className="relative">
+                                            <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                                                🤬 
+                                                <input 
+                                                    name="search"
+                                                    className='focus:ring-red-500 focus:border-rerder-red-500 block w-full pl-10 sm:text-sm border-gray-100 rounded-full p-2 border'
+                                                />
+                                            </div>
+                                        </div>
+                                        {/* search box end */}
+                                         <div className='relative rounded-lg px-2 py-2 flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 mb-3 hover:bg-gray-200'>
+                                                <div className='flex-shink-8'>
+                                                    <img 
+                                                        className='h-10 w-10 rounded-full'
+                                                        src={profile}
+                                                    />
+                                                </div>
+                                         </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
