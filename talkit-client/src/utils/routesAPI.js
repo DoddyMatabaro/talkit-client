3f06a697101eeabb81e60e8ebaf2f56de3184013
Doddy MATABARO <matabarododdy@gmail.com>
2022-11-14 00:28:12 +0200

    add api routes

---
+++ talkit-client/src/utils/routesAPI.js
@@ -0,0 +1,6 @@
+export const host = "http://localhost:9000";
+export const signupRoute = `${host}/api/auth/signup`;
+export const signinRoute = `${host}/api/auth/signin`;
+export const signoutRoute = `${host}/api/auth/signout`;
+export const sendRoute = `${host}/api/auth/send`;
+export const messagesRoute = `${host}/api/auth/messages`;
